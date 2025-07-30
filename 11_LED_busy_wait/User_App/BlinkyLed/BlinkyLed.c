68733ab76081c186ff5bd2318061d30f17188bf2
Max Koell <maxkoell@proton.me>
2025-07-30 17:14:26 +0200

    Refactored the Argument Checks on the input argument

--- 11_LED_busy_wait/User_App/BlinkyLed/BlinkyLed.c
+++ 11_LED_busy_wait/User_App/BlinkyLed/BlinkyLed.c
@@ -12,28 +12,33 @@
 
 #define MAX_PIN_COUNT_PER_PORT 20
 
-void BlinkyLed_Enable(BlinkyLed_Config_t *inout_pSelf) {
+void BlinkyLed_VerifyArg(BlinkyLed_Config_t *inout_pSelf)
+{
   ASSERT(inout_pSelf);
   ASSERT(inout_pSelf->pGpioPort);
   ASSERT(inout_pSelf->u16GpioPin < MAX_PIN_COUNT_PER_PORT);
+  ASSERT(inout_pSelf->eLedState <= E_BLINKY_LED_ON);
+}
+
+void BlinkyLed_Enable(BlinkyLed_Config_t *inout_pSelf)
+{
+  BlinkyLed_VerifyArg(inout_pSelf);
   HAL_GPIO_WritePin((GPIO_TypeDef *)inout_pSelf->pGpioPort,
                     (u16)inout_pSelf->u16GpioPin, GPIO_PIN_SET);
   inout_pSelf->eLedState = E_BLINKY_LED_ON;
 }
 
-void BlinkyLed_Disable(BlinkyLed_Config_t *inout_pSelf) {
-  ASSERT(inout_pSelf);
-  ASSERT(inout_pSelf->pGpioPort);
-  ASSERT(inout_pSelf->u16GpioPin < MAX_PIN_COUNT_PER_PORT);
+void BlinkyLed_Disable(BlinkyLed_Config_t *inout_pSelf)
+{
+  BlinkyLed_VerifyArg(inout_pSelf);
   HAL_GPIO_WritePin((GPIO_TypeDef *)inout_pSelf->pGpioPort,
                     (u16)inout_pSelf->u16GpioPin, GPIO_PIN_RESET);
   inout_pSelf->eLedState = E_BLINKY_LED_OFF;
 }
 
-void BlinkyLed_Toggle(BlinkyLed_Config_t *inout_pSelf) {
-  ASSERT(inout_pSelf);
-  ASSERT(inout_pSelf->pGpioPort);
-  ASSERT(inout_pSelf->u16GpioPin < MAX_PIN_COUNT_PER_PORT);
+void BlinkyLed_Toggle(BlinkyLed_Config_t *inout_pSelf)
+{
+  BlinkyLed_VerifyArg(inout_pSelf);
   inout_pSelf->eLedState = !inout_pSelf->eLedState;
   HAL_GPIO_WritePin((GPIO_TypeDef *)inout_pSelf->pGpioPort,
                     (u16)inout_pSelf->u16GpioPin,
